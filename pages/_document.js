2bee660e0b6014a5e5b20d624c8012d7476fc1a5
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-01-31 19:29:05 +0530

    src: added changes to add dynamically add data

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body>
+      <body className='bg-gradient-to-r from-orange-300 to-rose-300'>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
